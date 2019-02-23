82a2248d785d5f23237d0423f35f549a01bbb388
unknown <16tsmith55@gmail.com>
2019-02-23 11:48:58 -0700

    finished with split and NAN function

--- costello.js
+++ costello.js
@@ -1,4 +1,5 @@
 let myForm = document.getElementById("myForm");
+
 let radioLabel = document.getElementById("radioLabel");
 let nameLabel = document.getElementById("nameLabel");
 let phoneLabel = document.getElementById("phoneLabel");
@@ -6,12 +7,23 @@ let totalcost = document.getElementById("totalcost");
 let submitBtn = document.getElementById("submitBtn");
 let submitMessage = document.getElementById("submitMessage");
 let condition = true;
-var wait;
+let wait;
 
+function isANumber(num){
+  let arr = num.split("-");
+  if (arr[0].length != 3) return false;
+  if (arr[1].length != 3) return false;
+  if (arr[2].length != 4) return false;
+for(let i = 0; i < arr.length; i++){
+	if(isNaN(arr[i])) {
+    return false;
+    }
+  }
+    return true;
+  }
 function validateForm() {
   console.log("validating form");
   condition = true;
-  let phoneFormat = /^\d\d\d-\d\d\d-\d\d\d\d$/; //regex format
 
   if (myForm.pastatype.value == "") {
     radioLabel.innerText = "Required field! (You must choose a pasta)";
@@ -19,13 +31,13 @@ function validateForm() {
   } else {
     radioLabel.innerText = "";
   }
-  if (myForm.name.value == "") {
+  if (myForm.client_name.value == "") {
     nameLabel.innerText = "Required field!";
     condition = false;
   } else {
     nameLabel.innerText = "";
   }
-  if (myForm.phone.value == "" || !myForm.phone.value.match(phoneFormat)) {
+  if (myForm.phone.value == "" || !isANumber(myForm.phone.value)) {
     phoneLabel.innerText = "Enter in 888-888-8888 format!";
     condition = false;
   } else {
@@ -67,6 +79,7 @@ function calculateOrder() {
       myForm.submit();
       console.log("order sent!");
     }
+    console.log("didnt submit...");
   }, 5000);
 return false;
 }
